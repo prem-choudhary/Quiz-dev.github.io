cc3b7e751711947647668fe0f277ae55c8176a2d
Prem Choudhary <pk5480524@gmail.com>
2024-10-25 13:00:20 +0530

    set-Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import  { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // import "bootstrap/dist/css/bootstrop.min.css"
 import qBank from "./components/QuestionBank";
 import Questions from './components/Questions';
